a6fb178199c9a2a3cdbd908d88969bccced336f0
Paula Gramajo <paulagramajo.tuc@gmail.com>
2025-06-23 19:13:34 -0300

    realice cambios en el desarrollo del ejercicio 6

--- js/Ejercicio-06.js
+++ js/Ejercicio-06.js
@@ -1,10 +1,15 @@
 function calculoPertimetro(ladoA,ladoB){
+    if (ladoA>0 && ladoB >0){
     let p = 2 * (ladoA+ladoB);
     return p;
+    } else{
+        const error = 'no existe, debe ingresar numeros positivos'
+        return error
+    }
 }
 
 calculoPertimetro()
 const ladoA = parseInt(prompt('Ingrese el valor del lado A: '));
 const ladoB = parseInt(prompt('Ingrese el valor del lado B: '));
 const perimetro = calculoPertimetro(ladoA,ladoB)
-document.writeln(`El perimetro del cuadrado es ${perimetro}`)
+document.writeln(`Perimetro del rectangulo = ${perimetro}`)
